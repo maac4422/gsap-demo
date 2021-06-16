e06490cdc9a8c6373b9e61877320cc4a0dc450cc
Miguel Arenas <Miguel.Arenas@geniussports.com>
2021-06-15 21:04:17 -0500

    Expand and shrink circle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './App.css';
 import { TweenMax, Power3} from 'gsap';
 
 function App() {
-
+  let app = useRef(null)
   let circle = useRef(null)
   let circleRed = useRef(null)
   let circleBlue = useRef(null)
 
+  const [expandedCircle, setExpandedCircle] = useState(false)
+
+  const handleExpand = () => {
+    TweenMax.to(circleRed, .8, {width: 200, height: 200, ease: Power3.easeOut})
+    setExpandedCircle(true)
+  }
+
+  const handleShrink = () => {
+    TweenMax.to(circleRed, .8, {width: 75, height: 75, ease: Power3.easeOut})
+    setExpandedCircle(false)
+  }
+
   useEffect(() => {
+    TweenMax.to(app, 0, {visibility: 'visible'})
+    /*
     TweenMax.from(circle, .8, {opacity: 0, x: 40, ease: Power3.easeOut})
     TweenMax.from(circleRed, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: 2})
     TweenMax.from(circleBlue, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay:4})
+    */
+    TweenMax.staggerFrom([circle,circleRed,circleBlue], .8, {opacity: 0, x: 40, ease: Power3.easeOut}, .2)
   }, [])
+
+
   return (
-    <div className="App">
+    <div
+      ref={el => app = el}
+      className="App"
+    >
       <header className="App-header">
         <div className="cicle-container">
           <div
             ref={el=> circle = el}
             className="circle"></div>
            <div
+            onClick={expandedCircle ? handleShrink : handleExpand}
             ref={el=> circleRed = el}
             className="circle red"></div>
           <div
