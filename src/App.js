90dde1fe612a7b3a71cc502e1b7266d24c8b1c17
Miguel Arenas <Miguel.Arenas@geniussports.com>
2021-06-22 16:19:01 -0500

    Stadium svg

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useEffect, useRef, useState} from 'react'
 import './App.css';
 import { gsap, Power3} from 'gsap';
 import { CustomEase } from "gsap/CustomEase";
+import {ReactComponent as YourSvg} from './test-tube-svgrepo-com.svg'
+import { SvgIcon } from '@material-ui/core';
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
   let actionMatchRed = useRef(null)
   let actionMatchGray = useRef(null)
   let textEvent = useRef(null)
+  let arrowRoot = useRef(null)
+  let svgImage = useRef(null)
 
   gsap.registerPlugin(CustomEase);
 
@@ -43,6 +47,11 @@ function App() {
   const actionMatchEvent = () => {
     CustomEase.create("custom", "M0,0 C0.048,0.382 0.192,1.499 0.5,1.5 0.85,1.5 1,0 1,0 ");
     //TweenMax.to(actionMatchEventContainer ,{duration: 4 ,backgroundColor: "#6b5b95", ease: Power3.easeOut})
+
+
+    //Animacion de div
+
+    /*
     gsap.to(actionMatchBlue ,{duration: 1 ,width: "100%", ease: "custom",
       onComplete: function(){ 
         setTextNotification("TOUCH DOWN !!!!!")
@@ -56,9 +65,18 @@ function App() {
         setTextNotification("BUCCARESS")
       }
     })
+*/
+    
+    gsap.to(arrowRoot ,{duration: 5 ,width: "100%", ease: "custom", delay: 0})
+    //gsap.to(".talkbubble:before" ,{duration: 1 ,backgroundColor: "#6b5b95" , ease: "custom", delay: 0})
     
     
     
+  }
+
+  const changeSVG = () => {
+    
+    gsap.to(svgImage ,{duration: 2 ,fill: "rgb(255,0,255)", ease: "custom", delay: 0})
   }
 
 
@@ -85,22 +103,47 @@ function App() {
             <p className="team-name" ref={el=> textEvent = el}> 
               {textNotification}
             </p>
-            <div className="event-color blue" ref={el => actionMatchBlue = el}></div>
-            <div className="event-color gray" ref={el => actionMatchGray = el}></div>
-            <div className="event-color red" ref={el => actionMatchRed = el}></div>
+            <div  id="talkbubble" className="talkbubble" ref={el => arrowRoot = el}></div>
           </div>
 
           <div className="content-colors-left">
             
           </div>
-          
-        </div>
 
-        <div className="talkbubble">
+
+          <svg class="field" viewBox="0 0 120 60">
+  
+  <rect class="field__grass" x="0" y="0" width="120" height="60" fill="#5FA603"/>
+  <g class="field__competitor field__competitor--home">
+    <rect class="field__competitor-space" x="0" y="0" width="10" height="60" fill="blue" />
+    <text class="field__competitor-name" x="6" y="30" transform="rotate(-90,6,30)">HOME</text>
+  </g>
+  <g class="field__competitor field__competitor--away">
+    <rect class="field__competitor-space" x="110" y="0" width="10" height="60" fill="red" />
+    <text class="field__competitor-name" x="114" y="30" transform="rotate(90,114,30)">AWAY</text>
+  </g>
+  <rect class="field-border" x="0" y="0" width="120" height="60" fill="none" stroke="#FFF" stroke-width="2"/>
+  <path class="field-lines" d="M10 0 V60 M20 0 V60 M30 0 V60 M40 0 V60 M50 0 V60 M60 0 V60 M70 0 V60 M80 0 V60 M90 0 V60 M100 0 V60 M110 0 V60" stroke="#fff" stroke-width="0.25" />
+  
+  <line class="anim-line-1" x1="55" y1="1" x2="55" y2="59" stroke="#DBED67" stroke-width="1.5"/>
+  <line class="anim-line-2" x1="65" y1="1" x2="65" y2="59" stroke="#8AAAFD" stroke-width="1.5"/>
+  <path class="anim-curve" d="M 20 30 Q 40 10 60 30" stroke="red" stroke-dasharray="2 2" fill="transparent"/>
+  <circle class="anim-dot" cx="60" cy="30" r="1.5" fill="red" />
+  <polygon classes="anim-arrow" points="68,26 74,30 68,34" fill="red" />
+</svg>
         </div>
+        
+
+
+
+        
 
         <button onClick={actionMatchEvent}>
           TouchDown
+        </button>
+
+        <button onClick={changeSVG}>
+          Change SVG
         </button>
       </header>
     </div>
